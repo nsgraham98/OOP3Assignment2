689213d324e04241c74a16984255da935ac1a81f
Syruphie <ahmadfakhry1999@gmail.com>
2025-04-04 15:24:50 -0600

    Update MyStack.java's Documentation

--- src/implementations/MyStack.java
+++ src/implementations/MyStack.java
@@ -5,15 +5,36 @@ import java.util.NoSuchElementException;
 import utilities.Iterator;
 import utilities.StackADT;
 
+/**
+ * Implementation of the StackADT interface using an ArrayList.
+ * This class provides a LIFO (Last-In-First-Out) stack implementation
+ * using the MyArrayList class as the underlying data structure.
+ * 
+ * @param <E> The type of elements this stack holds.
+ * @version 1.0
+ * @see utilities.StackADT
+ * @see implementations.MyArrayList
+ */
 public class MyStack<E> implements StackADT<E> {
+    // The serialVersionUID for serializable class
     private static final long serialVersionUID = 1L; // 1 Long
-    
+
     private MyArrayList<E> stack;
 
+    /**
+     * Default constructor.
+     * Creates an empty stack with the default capacity.
+     */
     public MyStack() {
         this.stack = new MyArrayList<>();
     }
 
+    /**
+     * Pushes an element onto the top of this stack.
+     * 
+     * @param toAdd The element to be pushed onto this stack.
+     * @throws NullPointerException if the specified element is null.
+     */
     @Override
     public void push(E toAdd) throws NullPointerException {
         if (toAdd == null) {
@@ -22,6 +43,12 @@ public class MyStack<E> implements StackADT<E> {
         stack.add(toAdd);
     }
 
+    /**
+     * Removes and returns the element at the top of this stack.
+     * 
+     * @return The element at the top of this stack.
+     * @throws EmptyStackException if this stack is empty.
+     */
     @Override
     public E pop() throws EmptyStackException {
         if (isEmpty()) {
@@ -30,6 +57,12 @@ public class MyStack<E> implements StackADT<E> {
         return stack.remove(stack.size() - 1);
     }
 
+    /**
+     * Returns, but does not remove, the element at the top of this stack.
+     * 
+     * @return The element at the top of this stack.
+     * @throws EmptyStackException if this stack is empty.
+     */
     @Override
     public E peek() throws EmptyStackException {
         if (isEmpty()) {
@@ -38,16 +71,24 @@ public class MyStack<E> implements StackADT<E> {
         return stack.get(stack.size() - 1);
     }
 
+    // Removes all elements from this stack. The stack will be empty
     @Override
     public void clear() {
         stack.clear();
     }
 
+    // Returns true if this stack contains no elements
     @Override
     public boolean isEmpty() {
         return stack.isEmpty();
     }
 
+    /**
+     * Returns an array containing all of the elements in this stack in proper sequence
+     * (from the top of the stack to the bottom).
+     * 
+     * @return An array containing all of the elements in this stack in proper sequence.
+     */
     @Override
     public Object[] toArray() {
         Object[] array = new Object[stack.size()];
@@ -56,7 +97,18 @@ public class MyStack<E> implements StackADT<E> {
         }
         return array;
     }
-
+    
+    /**
+     * Returns an array containing all of the elements in this stack in proper sequence
+     * (from the top of the stack to the bottom); the runtime type of the returned array
+     * is that of the specified array.
+     * 
+     * @param holder The array into which the elements of this stack are to be stored,
+     *               if it is big enough; otherwise, a new array of the same runtime
+     *               type is allocated for this purpose.
+     * @return An array containing the elements of this stack.
+     * @throws NullPointerException if the specified array is null.
+     */
     @Override
     public E[] toArray(E[] holder) throws NullPointerException {
         if (holder == null) {
@@ -74,7 +126,14 @@ public class MyStack<E> implements StackADT<E> {
         }
         return holder;
     }
-
+    
+    /**
+     * Returns true if this stack contains the specified element.
+     * 
+     * @param toFind The element whose presence in this stack is to be tested.
+     * @return true if this stack contains the specified element.
+     * @throws NullPointerException if the specified element is null.
+     */
     @Override
     public boolean contains(E toFind) throws NullPointerException {
         if (toFind == null) {
@@ -82,7 +141,16 @@ public class MyStack<E> implements StackADT<E> {
         }
         return stack.contains(toFind);
     }
-
+    
+    /**
+     * Returns the 1-based position of the specified element in this stack,
+     * or -1 if this stack does not contain the element.
+     * The topmost element on the stack is considered to be at distance 1.
+     * 
+     * @param toFind The element to search for.
+     * @return The 1-based position of the element from the top of the stack,
+     *         or -1 if the element is not found.
+     */
     @Override
     public int search(E toFind) {
         for (int i = stack.size() - 1; i >= 0; i--) {
@@ -92,12 +160,26 @@ public class MyStack<E> implements StackADT<E> {
         }
         return -1;
     }
-
+    
+    /**
+     * Returns an iterator over the elements in this stack in proper sequence
+     * (from the top of the stack to the bottom).
+     * 
+     * @return An iterator over the elements in this stack in proper sequence.
+     */
     @Override
     public Iterator<E> iterator() {
         return new StackIterator();
     }
-
+    
+    /**
+     * Compares the specified stack with this stack for equality.
+     * Returns true if the specified object is a stack of the same type,
+     * has the same size, and contains the same elements in the same order.
+     * 
+     * @param that The stack to be compared for equality with this stack.
+     * @return true if the specified stack is equal to this stack.
+     */
     @Override
     public boolean equals(StackADT<E> that) {
         if (this == that) {
@@ -119,24 +201,32 @@ public class MyStack<E> implements StackADT<E> {
         return true;
     }
 
+    // Return the number of elements in this stack
     @Override
     public int size() {
         return stack.size();
     }
 
+    // Returns true if this stack is currently full
     @Override
     public boolean stackOverflow() {
         return false;
     }
-
+    
+    /**
+     * Private iterator implementation for MyStack.
+     * Provides a way to iterate through the elements of the stack from top to bottom.
+     */
     private class StackIterator implements Iterator<E> {
         private int currentIndex = stack.size() - 1;
 
+        // Returns true if the iteration has more elements
         @Override
         public boolean hasNext() {
             return currentIndex >= 0;
         }
 
+        // Returns the next element in the iteration
         @Override
         public E next() throws NoSuchElementException {
             if (!hasNext()) {
